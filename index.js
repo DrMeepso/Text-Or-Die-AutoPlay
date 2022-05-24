161320fb86d3b484ef64e2b1ee97f31b12f4cdb1
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2022-05-24 18:41:34 +1200

    Added Number Support And Made Inputs Synced

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function getScreenShot(){
 
     return new Promise( (res, rej) => {
 
-        exec(`adb.exe shell screencap /sdcard/devscreenshots/screen.png`, () => {
+        exec(`adb shell screencap /sdcard/devscreenshots/screen.png`, () => {
 
             exec(`del screen.png`)
-            exec(`adb.exe pull /sdcard/devscreenshots/screen.png screen.png`, () => {
+            exec(`adb pull /sdcard/devscreenshots/screen.png screen.png`, () => {
 
                 res("./screen.png")
 
@@ -41,26 +41,24 @@ getScreenShot()
         
         console.log("Reading Image Text")
         ReadText('./Question.png').then(text => {
+            
             var que = text.replaceAll("\n"," ").replaceAll("-","").replace(/\s+/g, ' ').trim().replaceAll(`’`,"")
-            var queandans = QuestionsJson.find( e => e.question.toLocaleLowerCase() == que.toLocaleLowerCase())
+            
+            var queandans = QuestionsJson.find( e => e.question.toLocaleLowerCase().replaceAll("-","") == que.toLocaleLowerCase())
             if(queandans == undefined){
-
-                var queandans = QuestionsJson.find( e => `${e.question.toLocaleLowerCase()} p` == que.toLocaleLowerCase())
-
-
+                var queandans = QuestionsJson.find( e => `${e.question.toLocaleLowerCase().replaceAll("-","")} p` == que.toLocaleLowerCase())
             }
-
 
             if (queandans == undefined){
 
                 console.log("Error Reading Question, Computor read it as: " + que)
 
                 var key = Keys.find(e => e.key == "n")
-                exec(`adb.exe shell input tap ${key.x} ${key.y}`)
+                exec(`adb shell input tap ${key.x} ${key.y}`)
                 setTimeout(() => {
                     
                     var key = Keys.find(e => e.key == "return")
-                    exec(`adb.exe shell input tap ${key.x} ${key.y}`)
+                    exec(`adb shell input tap ${key.x} ${key.y}`)
 
                 }, 500);
 
@@ -68,24 +66,10 @@ getScreenShot()
 
             }
 
-            console.log("Found Answers")
+            console.log("Found Answers to question: " + que)
 
-            var WrAns = queandans.answers.sort((a, b) => a.length - b.length)[queandans.answers.length-1].replaceAll(" ","").replaceAll("-","")
-            for (let i = 0; i < WrAns.length; i++) {
-                setTimeout(() => {
-                    
-                    var key = Keys.find(e => e.key == WrAns.split("")[i].toLocaleLowerCase())
-                    exec(`adb.exe shell input tap ${key.x} ${key.y}`)
-
-                }, i*1500);
-            }
-
-            setTimeout(() => {
-                
-                var key = Keys.find(e => e.key == "return")
-                exec(`adb.exe shell input tap ${key.x} ${key.y}`)
-
-            }, (WrAns.length*1500) + 1500);
+            var WrAns = Fix(queandans.answers.sort((a, b) => a.length - b.length)[queandans.answers.length-1].replaceAll(" ","").replaceAll("-",""))
+            TypeOnScreen(WrAns.split(""))
 
         }).catch(err => {
             console.log(err);
@@ -94,3 +78,68 @@ getScreenShot()
     }, 1500);
 
 })
+
+function TypeOnScreen(Text){
+
+    var textArray = Text
+
+    function DoOneChar(){
+
+
+        if (textArray[0] == undefined){
+
+            var key = Keys.find(e => e.key == "return")
+            exec(`adb shell input tap ${key.x} ${key.y}`)
+
+            return
+
+        }
+
+        var key = Keys.find(e => e.key == textArray[0].toLocaleLowerCase())
+        console.log("Pressing " + textArray[0].toLocaleLowerCase())
+        exec(`adb shell input tap ${key.x} ${key.y}`, () => {
+
+            textArray.shift()
+            DoOneChar()
+
+        })
+
+    }
+
+    DoOneChar()
+}
+
+function Fix(string){
+
+    return String(string)
+    .replaceAll("1","|1|")
+    .replaceAll("2","|2|")
+    .replaceAll("3","|3|")
+    .replaceAll("4","|4|")
+    .replaceAll("5","|5|")
+    .replaceAll("6","|6|")
+    .replaceAll("7","|7|")
+    .replaceAll("8","|8|")
+    .replaceAll("9","|9|")
+    .replaceAll("0","|0|")
+
+    .replaceAll("-","|-|")
+    .replaceAll("/","|/|")
+    .replaceAll(":","|:|")
+    .replaceAll(";","|;|")
+    .replaceAll("(","|(|")
+    .replaceAll(")","|)|")
+    .replaceAll("$","|$|")
+    .replaceAll("&","|&|")
+    .replaceAll("*","|*|")
+    .replaceAll("\"","|\"|")
+
+    .replaceAll(".","|.|")
+    .replaceAll(",","|,|")
+    .replaceAll("?","|?|")
+    .replaceAll("!","|!|")
+    .replaceAll("'","|'|")
+
+    .replaceAll("||","")
+
+}
